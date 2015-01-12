3b08fd892d77b54103f6bc06b1d3abad38c34e13
David R. Jenni <david.r.jenni@gmail.com>
2015-01-12 20:37:19 +0100

    Rearrange code.

--- agorn.go
+++ agorn.go
@@ -32,74 +32,6 @@ func (r bodyReader) Read(data []byte) (int, error) {
 	return r.Win.Read("body", data)
 }
 
-func openWin() (*acme.Win, error) {
-	id, err := strconv.Atoi(os.Getenv("winid"))
-	if err != nil {
-		return nil, err
-	}
-	return acme.Open(id, nil)
-}
-
-func readAddr(win *acme.Win) (q0, q1 int, err error) {
-	if _, _, err := win.ReadAddr(); err != nil {
-		return 0, 0, err
-	}
-	if err := win.Ctl("addr=dot"); err != nil {
-		return 0, 0, err
-	}
-	return win.ReadAddr()
-}
-
-func readFilename(win *acme.Win) (string, error) {
-	b, err := win.ReadAll("tag")
-	if err != nil {
-		return "", err
-	}
-	tag := string(b)
-	i := strings.Index(tag, " ")
-	if i == -1 {
-		return "", fmt.Errorf("cannot get filename from tag")
-	}
-	return tag[0:i], nil
-}
-
-func selection(win *acme.Win) (filename string, off int, err error) {
-	filename, err = readFilename(win)
-	if err != nil {
-		return "", 0, err
-	}
-	q0, _, err := readAddr(win)
-	if err != nil {
-		return "", 0, err
-	}
-	off, err = byteOffset(bufio.NewReader(&bodyReader{win}), q0)
-	if err != nil {
-		return "", 0, err
-	}
-	return
-}
-
-func byteOffset(r io.RuneReader, off int) (bo int, err error) {
-	for i := 0; i != off; i++ {
-		_, s, err := r.ReadRune()
-		if err != nil {
-			return 0, err
-		}
-		bo += s
-	}
-	return
-}
-
-func reloadShowAddr(win *acme.Win, off int) error {
-	if err := win.Ctl("get"); err != nil {
-		return err
-	}
-	if err := win.Addr("#%d", off); err != nil {
-		return err
-	}
-	return win.Ctl("dot=addr\nshow")
-}
-
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: agorn <name>\n")
@@ -128,4 +60,72 @@ func main() {
 		fmt.Fprintf(os.Stderr, "cannot restore selection: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+func openWin() (*acme.Win, error) {
+	id, err := strconv.Atoi(os.Getenv("winid"))
+	if err != nil {
+		return nil, err
+	}
+	return acme.Open(id, nil)
+}
+
+func selection(win *acme.Win) (filename string, off int, err error) {
+	filename, err = readFilename(win)
+	if err != nil {
+		return "", 0, err
+	}
+	q0, _, err := readAddr(win)
+	if err != nil {
+		return "", 0, err
+	}
+	off, err = byteOffset(bufio.NewReader(&bodyReader{win}), q0)
+	if err != nil {
+		return "", 0, err
+	}
+	return
+}
+
+func readFilename(win *acme.Win) (string, error) {
+	b, err := win.ReadAll("tag")
+	if err != nil {
+		return "", err
+	}
+	tag := string(b)
+	i := strings.Index(tag, " ")
+	if i == -1 {
+		return "", fmt.Errorf("cannot get filename from tag")
+	}
+	return tag[0:i], nil
+}
+
+func readAddr(win *acme.Win) (q0, q1 int, err error) {
+	if _, _, err := win.ReadAddr(); err != nil {
+		return 0, 0, err
+	}
+	if err := win.Ctl("addr=dot"); err != nil {
+		return 0, 0, err
+	}
+	return win.ReadAddr()
+}
+
+func byteOffset(r io.RuneReader, off int) (bo int, err error) {
+	for i := 0; i != off; i++ {
+		_, s, err := r.ReadRune()
+		if err != nil {
+			return 0, err
+		}
+		bo += s
+	}
+	return
+}
+
+func reloadShowAddr(win *acme.Win, off int) error {
+	if err := win.Ctl("get"); err != nil {
+		return err
+	}
+	if err := win.Addr("#%d", off); err != nil {
+		return err
+	}
+	return win.Ctl("dot=addr\nshow")
 }
