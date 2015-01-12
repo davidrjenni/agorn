b707294fa18e8df900947e8095abc967c59865e8
David R. Jenni <david.r.jenni@gmail.com>
2015-01-12 20:42:20 +0100

    Add license.

--- agorn.go
+++ agorn.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2014 David R. Jenni. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /*
 agorn is a wrapper around gorename for use with Acme.
 It renames the entity under the cursor.
